69baf86dd6954c4fdb59dce94fcf459a8feeaf5e
Sarkar <rohisarkar@deloitte.com>
2025-06-07 18:57:22 +0530

    testing with optional & default data

--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,15 +1,19 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
+#Optional for Optional Value
+#Field for Default Value
 class EventSchema (BaseModel):
     id: int
+    page: Optional[str] = ""
+    description: Optional[str] = Field(default='My Description')
 
 class EventListSchema(BaseModel):
     results: List[EventSchema]
     count: int
 
 class EventCreateSchema(BaseModel):
-    page:str
+    page: str
 
 class EventUpdateSchema(BaseModel):
     description: str
