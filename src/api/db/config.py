38df83c334619ddcec13d81d7c409616fd8af6d4
Sarkar <rohisarkar@deloitte.com>
2025-06-12 02:12:49 +0530

    creating hypertables with timescaledb

--- src/api/db/config.py
+++ src/api/db/config.py
@@ -3,4 +3,6 @@
 from decouple import config as decouple_config
 
 
-DATABASE_URL= decouple_config("DATABASE_URL",default="")
+# DATABASE_URL= decouple_config("DATABASE_URL",default="")
+DATABASE_URL="postgresql+psycopg://time-user:time-pw@db_service:5432/timescaledb"
+DB_TIMEZONE = decouple_config("DB_TIMEZONE", default="UTC")
--- src/api/events/models.py
+++ src/api/events/models.py
@@ -4,28 +4,34 @@ from typing import List, Optional
 from datetime import timezone, datetime
 import sqlmodel
 from sqlmodel import SQLModel, Field
+from timescaledb import TimescaleModel
+from timescaledb.utils import get_utc_now
 
-def get_utc_now():
-    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
+# def get_utc_now():
+#     return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
 
 
 #Optional for Optional Value
 #Field for Default Value
-class EventModel (SQLModel,table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class EventModel (TimescaleModel,table=True):
+    # id: Optional[int] = Field(default=None, primary_key=True)
     # id:int
-    page: Optional[str] = ""
+    # page: Optional[str] = ""
+    page:str= Field(index=True)
     description: Optional[str] = ""
-    created_at: datetime = Field(
-        default_factory= get_utc_now,
-        sa_type = sqlmodel.DateTime(timezone=True),
-        nullable=False
-    )
+    # created_at: datetime = Field(
+    #     default_factory= get_utc_now,
+    #     sa_type = sqlmodel.DateTime(timezone=True),
+    #     nullable=False
+    # )
     updated_at: datetime = Field(
         default_factory= get_utc_now,
         sa_type = sqlmodel.DateTime(timezone=True),
         nullable=False
     )
+
+    __chunk_time_interval__ = "INTERVAL 1 day"
+    __drop_after__ = "INTERVAL 3 days"
 
 class EventListSchema(SQLModel):
     results: List[EventModel]
